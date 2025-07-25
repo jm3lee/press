44756b8cd4b051dd6b4bb6863f2e81b00a26b6b7
jm3lee <39203409+jm3lee@users.noreply.github.com>
2025-07-25 11:31:11 -0700

    Add tests for get_tracking_options (#52)

--- app/shell/py/pie/tests/test_render_template.py
+++ app/shell/py/pie/tests/test_render_template.py
@@ -12,3 +12,27 @@ def test_override_class():
     desc = {"citation": "foo", "url": "/f", "link": {"class": "external"}}
     html = render_template.linktitle(desc)
     assert 'class="external"' in html
+
+
+def test_tracking_false_adds_attributes():
+    desc = {"link": {"tracking": False}}
+    opts = render_template.get_tracking_options(desc)
+    assert opts == 'rel="noopener noreferrer" target="_blank"'
+
+
+def test_tracking_true_returns_empty():
+    desc = {"link": {"tracking": True}}
+    opts = render_template.get_tracking_options(desc)
+    assert opts == ""
+
+
+def test_no_link_returns_empty():
+    desc = {"citation": "foo"}
+    opts = render_template.get_tracking_options(desc)
+    assert opts == ""
+
+
+def test_missing_tracking_interpreted_as_false():
+    desc = {"link": {}}
+    opts = render_template.get_tracking_options(desc)
+    assert opts == 'rel="noopener noreferrer" target="_blank"'
