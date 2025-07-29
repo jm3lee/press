ebbf79b9f8852d6a17604543e113c948e09e76dc
Brian Lee <jm3lee@gmail.com>
2025-07-28 17:52:52 -0700

    fix test failure

--- app/shell/py/pie/tests/test_picasso.py
+++ app/shell/py/pie/tests/test_picasso.py
@@ -7,6 +7,7 @@ def test_generate_rule_basic():
     rule = picasso.generate_rule(Path("src/foo/bar.yml")).strip()
     expected = (
         "build/foo/bar.yml: src/foo/bar.yml\n"
+        "\t$(Q)mkdir -p $(dir build/foo/bar.yml)\n"
         "\t$(Q)emojify < $< > $@\n"
         "\t$(Q)process-yaml $< $@\n"
         "build/foo/bar.html: build/foo/bar.md build/foo/bar.yml\n"
