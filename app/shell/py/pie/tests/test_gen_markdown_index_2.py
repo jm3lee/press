f8120ad18b42fe1c71969ffec180fd066f67b0ed
Brian Lee <jm3lee@gmail.com>
2025-08-06 14:03:11 -0700

    fix pytest

--- app/shell/py/pie/tests/test_gen_markdown_index_2.py
+++ app/shell/py/pie/tests/test_gen_markdown_index_2.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import subprocess
 
-SCRIPT = Path(__file__).resolve().parents[3] / "bin" / "gen-markdown-index-2"
+SCRIPT = Path("/app") / "bin" / "gen-markdown-index-2"
 
 
 def test_show_property(tmp_path):
