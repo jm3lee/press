1381c45a0384d6d1262b0e013051594c4122c55a
jm3lee <39203409+jm3lee@users.noreply.github.com>
2025-08-11 15:43:28 -0700

    refactor bin scripts to use shared shell helpers (#275)

--- app/shell/sh/lib.sh
+++ app/shell/sh/lib.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-press_bash() {
+press_run() {
   docker compose run \
-    --rm --entrypoint bash -u "$(id -u)" \
-    shell "$@"
+    --rm --entrypoint "$1" -u "$(id -u)" \
+    shell "${@:2}"
+}
+
+press_bash() {
+  press_run bash "$@"
+}
+
+press_make() {
+  press_run make -f /app/mk/build.mk "$@"
+}
+
+press_redis_cli() {
+  docker compose exec dragonfly redis-cli "$@"
 }
 
