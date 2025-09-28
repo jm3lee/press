e79f846fd49e6930aca14382c1daf170385d8bee
Brian Lee <jm3lee@gmail.com>
2025-09-28 16:56:33 -0700

    squash

--- app/flashoffer-demo/dep.mk
+++ app/flashoffer-demo/dep.mk
@@ -6,17 +6,17 @@ FLASHOFFER_DEMO_SRC := $(shell find app/flashoffer-demo/src -type f)
 all: build/static/js/flashoffer-demo.js
 
 build/static/js/flashoffer-demo.js: app/build/static/js/flashoffer-demo.js | build/static/js
-cp $< $@
+	cp $< $@
 
 app/build/static/js/flashoffer-demo.js: $(FLASHOFFER_DEMO_SRC) \
-app/flashoffer-demo/index.html \
-app/flashoffer-demo/vite.config.ts \
-app/flashoffer-demo/tsconfig.json \
-app/flashoffer-demo/tsconfig.node.json \
-app/flashoffer-demo/package.json \
-app/flashoffer-demo/.init
-cd app/flashoffer-demo; npm run build
+	app/flashoffer-demo/index.html \
+	app/flashoffer-demo/vite.config.ts \
+	app/flashoffer-demo/tsconfig.json \
+	app/flashoffer-demo/tsconfig.node.json \
+	app/flashoffer-demo/package.json \
+	app/flashoffer-demo/.init
+	cd app/flashoffer-demo; npm run build
 
 app/flashoffer-demo/.init:
-cd app/flashoffer-demo; npm install
-touch $@
+	cd app/flashoffer-demo; npm install
+	touch $@
