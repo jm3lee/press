748982af27c68b7a1efcb0eec1f46c1ed8112ba2
jm3lee <39203409+jm3lee@users.noreply.github.com>
2025-10-22 13:25:23 -0700

    chore: add prettier tooling to quiz manager (#772)

---
+++ app/flashoffer/quiz-manager/prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Flashoffer Developers.
+ * Licensed under the MIT license.
+ */
+
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+};
